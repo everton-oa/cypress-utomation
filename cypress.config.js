b69c010d77eff7c3d3d81211f617d60303635b68
Everton Araujo <64025923+everton-oa@users.noreply.github.com>
2024-11-27 07:01:44 +0000

    Practice Cypress exercises from Class 50 through Class 59 (#6)
    * :construction: Class 51 - Using fixtures to load data
    
    * :construction: Class 52 - Update timeout in global configs
    
    * :recycle: Page Object refactoring
    
    * :construction: Add Mochawsome reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: "cypress/integration/examples/*.js",
   },
 });
