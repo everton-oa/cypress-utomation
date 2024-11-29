a1876defe9db06776454e57d89479a2abaafdaf9
Everton Araujo <everton.oa@gmail.com>
2024-11-29 17:10:55 +0000

    :construction: Class 86 - E2E Purchase

--- cypress/integration/examples/class87-session-test.js
+++ cypress/integration/examples/class87-session-test.js
@@ -9,5 +9,17 @@ describe("Test mocking HTTP response", function () {
         },
       });
     });
+    cy.get(".card-body button:last-of-type").eq(1).click();
+    cy.get("[routerlink*='cart']").click();
+    cy.contains("Checkout").click();
+    cy.get("[placeholder*='Country']").type("Ind");
+    cy.get(".ta-results button").each(($el, index, $list) => {
+      if ($el.text() === " India") {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get(".action__submit").click();
+    cy.wait(2000);
+    cy.get(".order-summary button").click();
   });
 });
