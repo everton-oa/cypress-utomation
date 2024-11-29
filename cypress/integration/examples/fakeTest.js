447079ac0d8bfb19bc47f54f0599b5e53d95880c
Everton Araujo <everton.oa@gmail.com>
2024-11-29 14:46:26 +0000

    :construction: Class 76  - HTTP responses assertion

--- cypress/integration/examples/fakeTest.js
+++ cypress/integration/examples/fakeTest.js
@@ -22,7 +22,9 @@ describe("Test mocking HTTP response", function () {
     ).as("bookRetrievals");
 
     cy.get("button[class='btn btn-primary']").click();
-    cy.wait("@bookRetrievals");
+    cy.wait("@bookRetrievals").then(({ request, response }) => {
+      cy.get("tr").should("have.length", response.body.length + 1);
+    });
     cy.get("p").should("have.text", "Oops only 1 Book available");
   });
 });
