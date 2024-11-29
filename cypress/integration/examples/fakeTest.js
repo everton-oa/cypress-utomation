646043ec35b9cb60d06a2d6f9e1d9830ccc791d9
Everton Araujo <everton.oa@gmail.com>
2024-11-29 14:30:06 +0000

    :construction: Class 76  - Mock HTTP responses

---
+++ cypress/integration/examples/fakeTest.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe("Test mocking HTTP response", function () {
+  it("Test Case", function () {
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
+
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty",
+      },
+      {
+        statusCode: 200,
+        body: [
+          {
+            book_name: "Learn Appium Automation with Java code",
+            isbn: "RS189",
+            aisle: "225",
+          },
+        ],
+      }
+    ).as("bookRetrievals");
+
+    cy.get("button[class='btn btn-primary']").click();
+    cy.wait("@bookRetrievals");
+    cy.get("p").should("have.text", "Oops only 1 Book available");
+  });
+});
